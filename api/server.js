7656c570a710f1f1da7ff5a61b8d31eab91fe368
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-24 23:14:18 -0400

    inserted get endpoint, error handling middleware, added missing part of syntax

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 
 const restrict = require('./middleware/restricted.js');
 
-const authRouter = require('./auth/auth-router.js');
+const authRouter = require('./auth/auth-router');
 const jokesRouter = require('./jokes/jokes-router.js');
 
 const server = express();
@@ -15,5 +15,16 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+
+server.get("/", (req, res) => {
+    res.json({ api: "up" })
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
 
 module.exports = server;
