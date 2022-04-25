265733551bd2b19c2b90b11374579b616a44b911
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-25 00:50:45 -0400

    restricted middleware in function

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,8 @@
+const { JWT_SECRET } = require('../secrets/index.js')
+const { findBy } = require('../users/user-model')
+const jwt = require('jsonwebtoken')
+
 module.exports = (req, res, next) => {
-  next();
   /*
     IMPLEMENT
 
@@ -11,4 +14,20 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
+ const token = req.headers.authorization
+ if (!token) {
+   res.status(401).json({
+     message: "token required"
+   })
+ }
+ jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+   if (err) {
+     res.status(401).json({
+       message: "token invalid"
+     })
+   } else {
+     req.decodedToken = decodedToken
+     next()
+   }
+ })
 };
