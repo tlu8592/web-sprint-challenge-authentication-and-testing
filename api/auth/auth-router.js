41adbe464dadfb72301518d67e2d253f648e6a1b
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-25 00:26:44 -0400

    auth endpoint: /login in function

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
-const { checkMissingInputs, checkUsernameTaken } = require('../middleware/validationMiddleware');
+const { JWT_SECRET } = require('../secrets/index')
+const { 
+  checkMissingInputs, 
+  checkUsernameTaken
+} = require('../middleware/validationMiddleware');
 const User = require('../users/user-model')
 
 router.post('/register', checkMissingInputs, checkUsernameTaken, (req, res, next) => {
@@ -40,8 +44,11 @@ router.post('/register', checkMissingInputs, checkUsernameTaken, (req, res, next
       .catch(next)
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post(
+  '/login',
+  checkMissingInputs, 
+  (req, res, next) => {
+  // res.end('implement login, please!');
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -65,6 +72,35 @@ router.post('/login', (req, res) => {
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
+  const { username, password } = req.body
+  User.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = buildToken(user)
+          res.json({ 
+            message: `welcome, ${user.username}`,
+            token
+          })
+        } else {
+          res.status(401).json({ 
+            message: "invalid credentials"
+          })
+        }
+      })
+      .catch(next)
+  
 });
+
+function buildToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, JWT_SECRET, options)
+}
 
 module.exports = router;
--- api/middleware/validationMiddleware.js
+++ api/middleware/validationMiddleware.js
@@ -26,7 +26,24 @@ async function checkUsernameTaken (req, res, next) {
     }
 }
 
+// async function checkUsernameDataExists (req, res, next) {
+//     try {
+//         const user = await User.findBy({ username: req.body.username })
+//         if (user.length) {
+//             req.user = user[0]
+//             next()
+//         } else {
+//             res.status(401).json({
+//                 message: "invalid credentials"
+//             })
+//         }
+//     } catch (err) {
+//         next(err)
+//     }
+// }
+
 module.exports = {
     checkMissingInputs,
-    checkUsernameTaken
+    checkUsernameTaken,
+    // checkUsernameDataExists
 }
